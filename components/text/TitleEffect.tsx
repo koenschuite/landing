cc0620f2506d7c97a2549543d31672f16168d709
koenschuite <tse.lao@yahoo.com>
2023-07-20 09:11:57 +0200

    fixing

--- components/text/TitleEffect.tsx
+++ components/text/TitleEffect.tsx
@@ -1,27 +1,38 @@
 "use client";
+import { useEffect } from "react";
 import "./TitleEffect.css";
 
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 export default function TitleEffect() {
-  document.querySelector("h1")?.addEventListener("mouseover", (event:any) => {
-    let iterations = 0;
-    
-    const interval = setInterval(() => {
-      event.target.innerText = event.target.innerText
-        .split("")
-        .map((letter:any, index:number) => {
-          if(index < iterations){
-              return event.target.dataset.value[index];
-          }
-          return letters[Math.floor(Math.random() * 26)]
-        })
-        .join("");
+  useEffect(() => {
+    const h1Element = document.querySelector("h1");
+
+    if (h1Element) {
+      h1Element.addEventListener("mouseover", (event:any) => {
+        let iterations = 0;
+        const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
         
-        if(iterations >= event.target.dataset.value.length) clearInterval(interval);
-        
-        iterations += 1/3;
-    }, 50);
-  });
+        const interval = setInterval(() => {
+          event.target.innerText = event.target.innerText
+            .split("")
+            .map((letter:any, index:number) => {
+              if(index < iterations){
+                  return event.target.dataset.value[index];
+              }
+              return letters[Math.floor(Math.random() * 26)];
+            })
+            .join("");
+            
+            if(iterations >= event.target.dataset.value.length) clearInterval(interval);
+            
+            iterations += 1/3;
+        }, 50);
+      });
+    }
+  }, []);
+
+
+
+
   return <h1 data-value="DATAPONTE" className="title-effect">DATAPONTE</h1>;
 }
